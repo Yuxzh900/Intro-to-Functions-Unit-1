a945911873d2c002b6817f05f3ece256b0e2729e
Yuxzh900 <yuxiangz6@nycstudents.net>
2026-09-15 06:20:04 -0700

    Unit One Commit 9/14/2026

--- app.py
+++ app.py
@@ -4,5 +4,21 @@ t = Turtle()
 
 t.shape('turtle')
 
-t.forward(200)
+# t.forward(200)
+
+def square (x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(200)
+
+def message(input):
+    print(input)
+message("The turtle and the rabbit")
+
 turtle.done()
