5cffb2dc33bcc139246db52a09574c1cf24e2fd2
Yuxzh900 <yuxiangz6@nycstudents.net>
2026-09-16 06:19:33 -0700

    Unit One 9/16/2026

--- app.py
+++ app.py
@@ -6,16 +6,34 @@ t.shape('turtle')
 
 # t.forward(200)
 
-def square (x):
-    t.forward(x)
+# def square (x):
+#     t.forward(x)
+#     t.left(90)
+#     t.forward(x)
+#     t.left(90)
+#     t.forward(x)
+#     t.left(90)
+#     t.forward(x)
+#     t.left(90)
+# square(200)
+
+# def triangle (y):
+#     t.forward(y)
+#     t.left(120)
+#     t.forward(y)
+#     t.left(120)
+#     t.forward(y)
+#     t.left(120)
+# triangle(200)
+
+def Righttriangle():
     t.left(90)
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-square(200)
+    t.forward(142)
+    t.left(135)
+    t.forward(200)
+    t.left(135)
+    t.forward(142)
+Righttriangle()
 
 def message(input):
     print(input)
