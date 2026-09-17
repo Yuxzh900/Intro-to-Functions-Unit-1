97584e69de43176c916a3ee6db6ac4c73f4e0a4c
Yuxzh900 <yuxiangz6@nycstudents.net>
2026-09-17 06:20:11 -0700

    Unit One 9/17/2026

--- loop.py
+++ loop.py
@@ -1,2 +1,26 @@
-for i in range(3):
-    print(i)
+import turtle
+from turtle import *
+t = Turtle()
+
+t.shape('turtle')
+
+t.speed(10)
+
+# #Square
+# for i in range(4):
+#     t.forward(200)
+#     t.left(90)
+
+
+# #Triangle
+# for i in range(3):
+#     t.forward(200)
+#     t.left(120)
+
+for i in range(60):
+    for i in range(4):
+        t.left(90)
+        t.forward(200)
+    t.right(5)
+
+turtle.done()
