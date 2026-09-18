fffd233fd7a0a6771bffa6d6df6f863595cfa9ad
Yuxzh900 <yuxiangz6@nycstudents.net>
2026-09-18 06:20:40 -0700

    Unit One 9/18/2026

--- loop.py
+++ loop.py
@@ -17,10 +17,27 @@ t.speed(10)
 #     t.forward(200)
 #     t.left(120)
 
-for i in range(60):
+# def square(x):
+#     for i in range(60):
+#         for i in range(4):
+#             t.forward(x)       
+#             t.left(90)
+#         t.right(5)
+# square(200)
+
+sidelength = 100
+rotate = 90
+def square(x,y):
     for i in range(4):
-        t.left(90)
-        t.forward(200)
-    t.right(5)
+        t.forward(x)
+        t.left(y)
+square(100,90)
+
+def doublingsquares(irange):
+    length = (25)
+    for i in range(irange):
+        square(length, 90)
+        length = length * 2
+doublingsquares(5)
 
 turtle.done()
---
+++ note.py
@@ -0,0 +1,21 @@
+#Strings for characters
+name="oo"
+print (name.upper())
+#input asks the user a question and records the answer
+#what we write in the input arguemt is what the user sees
+#input always outputs a spring
+bill=input("how much was the bill")
+print(bill)
+
+if bill==10:
+    print("match")
+else:print ("no match")
+
+# #integers for whole number
+# amount = 100
+# #Float uses decimal
+# amt_two = 99.99
+
+#BooLean
+x = True
+y = False
