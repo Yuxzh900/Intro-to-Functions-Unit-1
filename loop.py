04ae1da53a977bca360ead15320acd661db76099
Yuxzh900 <yuxiangz6@nycstudents.net>
2026-09-23 05:45:18 -0700

    Unit One (9/22/2026)

--- loop.py
+++ loop.py
@@ -31,13 +31,28 @@ def square(x,y):
     for i in range(4):
         t.forward(x)
         t.left(y)
-square(100,90)
+square(200,90)
 
-def doublingsquares(irange):
-    length = (25)
+# def doublingsquares(irange):
+#     length = (25)
+#     for i in range(irange):
+#         square(length, 90)
+#         length = length * 2
+# doublingsquares(5)
+
+# def addingsqaures(irange):
+#     length = 25
+#     for i in range(irange):
+#         square (length, 90)
+#         length += 25
+# addingsqaures(5)
+
+def sqaurespiral(irange):
+    length = 5
     for i in range(irange):
-        square(length, 90)
-        length = length * 2
-doublingsquares(5)
+        square (length, 90) 
+        length += 5
+        t.right(5)
+sqaurespiral(60)
 
 turtle.done()
